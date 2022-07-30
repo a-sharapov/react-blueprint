334602ea071ddc23cbb1fd416576320724f430d2
Alexander Sharapov <a-sharapov@outlook.com>
2022-07-30 18:32:52 +0300

    Updates

--- src/views/NotFound/index.tsx
+++ src/views/NotFound/index.tsx
@@ -1,12 +1,20 @@
 import { FC } from 'react';
 import BaseLayout from 'layouts/BaseLayout';
+import { Link } from 'react-router-dom';
 import './styles.css';
 
 const NotFound: FC = () => {
   return (
-    <BaseLayout title="Not Found">
-      <h1>404</h1>
-      <p>Page not found</p>
+    <BaseLayout title="Page not Found">
+      <section id="error-page">
+        <h1 className="error-page-header">Ooops! 404</h1>
+        <p>Seems like the page you are looking for does not exist.</p>
+        <p>&nbsp;</p>
+        <h5>But you are always might:</h5>
+        <p>
+          <Link to="/">Return to Homepage</Link>
+        </p>
+      </section>
     </BaseLayout>
   );
 };
