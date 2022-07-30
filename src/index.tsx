f5141958e332cb4f83b08c3933a64ba437164c89
Alexander Sharapov <a-sharapov@outlook.com>
2022-07-30 21:09:07 +0300

    Correcting false positives of ESLint

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+/*eslint-disable */
 import { registerSW } from 'virtual:pwa-register';
 
 const updateSW = registerSW({
   onRegisterError(error) {},
   onNeedRefresh() {},
-  onOfflineReady() {},
+  onOfflineReady() {}
 });
+/* eslint-enable */
 
 const root = ReactDOM.createRoot(document.querySelector('main#app-root') as HTMLElement);
 root.render(
