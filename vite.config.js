46f4afe0d90a54220e18919b32310ec86e6ed5ae
Alexander Sharapov <a-sharapov@outlook.com>
2022-07-30 17:43:49 +0300

    Fixes

--- vite.config.js
+++ vite.config.js
@@ -11,15 +11,27 @@ export default defineConfig(({ command, mode }) => {
       outDir: env.VITE_OUTPUT_PATH,
       rollupOptions: {
         external: [/^node:.*/, /^vite:.*/],
+        output: {
+          chunkFileNames: 'assets/js/[name]-[hash].js',
+          entryFileNames: 'assets/js/[name]-[hash].js',
+
+          assetFileNames: ({ name }) => {
+            if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')) {
+              return 'assets/images/[name]-[hash][extname]';
+            }
+
+            if (/\.css$/.test(name ?? '')) {
+              return 'assets/css/[name]-[hash][extname]';
+            }
+            return 'assets/[name]-[hash][extname]';
+          },
+        },
       },
     },
     publicDir: env.VITE_PUBLIC_PATH,
     root: env.VITE_ROOT_PATH,
     server: {
       port: env.VITE_APP_PORT,
-      fs: {
-        allow: ['..'],
-      },
     },
     plugins: [
       react(),
